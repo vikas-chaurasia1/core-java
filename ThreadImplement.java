506ece6dc748e166a6b76ed6ca5dbf21a382e377
vikas-chaurasia1 <46617082+vikas-chaurasia1@users.noreply.github.com>
2026-04-18 14:14:27 +0530

    Update ThreadImplement.java
    add executor service and Completable future

--- ThreadImplement.java
+++ ThreadImplement.java
@@ -5,8 +5,15 @@ import java.util.concurrent.*;
 class TestCallable implements Callable<String>{
 
     @Override
-    public String call(){
+    public String call() throws InterruptedException {
+        Thread.sleep(9000);
         return "value return from call method";
+    }
+}
+
+class TestCompletableFuture{
+    public int testCompltFut(){
+        return 10;
     }
 }
 
@@ -19,6 +26,30 @@ public class ThreadImplement {
         TestCallable testCallObj=new TestCallable();
         Future<String> future=ex.submit(testCallObj);
         System.out.println(future.get());
+
+
+        CompletableFuture<Integer> completableFuture=CompletableFuture.supplyAsync(()->{
+            System.out.println("Completable future");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return 10;
+        },ex).thenApply(x->x*27);
+
+        completableFuture.thenAccept(x-> System.out.println("vlaue of x->"+x));
+        System.out.println("checking completable future blocking or not");
+
+
+        TestCompletableFuture obj2=new TestCompletableFuture();
+        CompletableFuture<Integer> completableFuture1=CompletableFuture.supplyAsync(()->{
+            System.out.println("testing with executor");
+            return obj2.testCompltFut();
+        },ex).thenApply(x->x*10);
+        completableFuture1.thenAccept(y-> System.out.println("final result using ex service "+y));
+
+        completableFuture.join();
         ex.shutdown();
     }
 }
